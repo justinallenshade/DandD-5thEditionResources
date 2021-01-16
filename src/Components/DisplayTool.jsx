f144873dc1e3cbdba35dc7f71da8ed46bf9205b4
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-01-16 03:45:45 -0600

    2nd call pulling info, need to display

--- src/Components/DisplayTool.jsx
+++ src/Components/DisplayTool.jsx
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from 'react';
-let arrName = [];
-let urlCondition = 0;
-// let urlLayer2 = "https://www.dnd5eapi.co"
 
+
+let arrName = [];
+let arrUrls = [];
+
+let urlCondition = 0;
+let layer2 = "https://www.dnd5eapi.co"
+
+function urlCall2(url2){
+    fetch(url2)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        arrUrls.push(res.name);
+    })
+    .catch(err => console.log(`failed to seduce the dragon----- ${err}`))
+    console.log(`gate 2 passed`)
+}
 
 async function urlCall(url){
     await fetch(url)
     .then(res => res.json())
     .then(res => {
         let arrMain = res.results
+        
 
         let k=0;
         arrName = arrMain.map(i => 
         {return(
+            
             <div key={k = k+1} className="results">
-                <p>{i.name}</p>
-                {/* <p>{i.url}</p> */}
+                <p>{i.name}</p> 
+                {urlCall2(layer2 + i.url)}
+                <p>{arrUrls}</p>
+                
             </div>
+            
         )});
-        
-        console.log(arrMain.map(i=> i.name));
-        // console.log(url)
-
-        
-        
+        // console.log(arrUrls)
     })
+   
     .catch(err => console.log(`Your spell fizzled out!- ${err}`))
+
+    
 
     console.log(`gate 1 passed`)
     urlCondition = url
@@ -42,6 +59,7 @@ export default function DisplayTool({ url }) {
     if(urlCondition === 0 || urlCondition !== url){
         console.log(`conditional ran`)
         urlCall(url);
+        arrUrls =[]
     }
     
     const [state, setState] = useState([])
@@ -49,15 +67,17 @@ export default function DisplayTool({ url }) {
         setTimeout(() => {
             console.log(`use effect ran`)
             setState(arrName);
-        }, 1000);
+        }, 10000);
     },[]);
    
 
     console.log(`reached the return------------------------`);
+    
     return (
         
         <div className="wholeResults">
             {state}
+           
         </div>
     );
 }
